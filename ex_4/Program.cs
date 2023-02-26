39291a9726893ad474228f06b9e7d21206bed160
Yasha <yashasidin333@yandex.ru>
2023-02-26 15:32:20 +0300

    Add ex_4

---
+++ ex_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 22, 333, 43, 544, 644, 74, 84, 91};
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Наше число на позиции: {index}");
+    }
+    index += 1;
+}
