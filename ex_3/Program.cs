d8cea96ff36539f8b6c31a9175bf210e2078667f
Yasha <yashasidin333@yandex.ru>
2023-02-26 15:22:27 +0300

    Add ex_3

---
+++ ex_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// max from nine numbers using function
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//             0   1   2    3   4    5    6   7   8
+int[] array = {1, 22, 333, 43, 544, 644, 74, 84, 91};
+
+
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine($"max = {max}");
+
